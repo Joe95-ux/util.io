9bb1e9ce66e3b8a5e1bc2ad1f547e25152d36941
joe95-ux <ogorktabi@gmail.com>
2024-11-02 22:50:49 +0200

    add general menu

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,7 +1,11 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import {siteConfig} from "@/config/site";
-import { ThemeProvider } from "@/components/theme-provider"
+import { siteConfig } from "@/config/site";
+import { ThemeProvider } from "@/components/theme-provider";
+import {ModeToggle} from "@/components/mode-toggle";
+import {MobileSidebar} from "@/components/mobile-sidebar";
+import {Sidebar} from "@/components/sidebar";
+import { Search, Music, User2Icon } from "lucide-react";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -18,7 +22,7 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: {
     default: siteConfig.name,
-    template: `%s | ${siteConfig.name}`
+    template: `%s | ${siteConfig.name}`,
   },
   description: siteConfig.description,
   icons: [
@@ -39,14 +43,39 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
+        <div className="flex min-h-screen min-w-full bg-background max-h-screen">
+          <div className="h-[65px] md:pl-80 fixed inset-y-0 w-full z-50">
+            <nav className="flex justify-between items-center border-b border-border px-4 py-4">
+              <MobileSidebar />
+              <div className="hidden md:flex w-72 bg-transparent dark:bg-transparent relative pointer-events-auto">
+                <button className="w-full lg:flex items-center text-sm leading-6 transition-colors text-slate-400 rounded-md ring-1 ring-cyan-300/20 shadow-sm py-1.5 pl-2 pr-3 hover:ring-cyan-300/40 dark:bg-inherit dark:highlight-white/5 dark:hover:bg-inherit">
+                  <Music size={18} className="mr-6" />
+                  Search Utils.io...
+                  <span className="ml-auto pl-3 flex-none text-xs font-semibold">
+                    <Search size={18} />
+                  </span>
+                </button>
+              </div>
+              <div className="flex gap-4 items-center">
+                <ModeToggle />
+                <User2Icon size={18} className="ml-6"  />
+              </div>
+            </nav>
+          </div>
+          <div className="hidden md:flex h-full w-80 flex-col fixed inset-y-0 z-50">
+            <Sidebar />
+          </div>
+          <main className="md:pl-80 pt-[65px] flex h-full w-full flex-grow">
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              {children}
+            </ThemeProvider>
+          </main>
+        </div>
       </body>
     </html>
   );
